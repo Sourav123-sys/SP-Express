65d6691983e6a7da98c49ecc76d2533bce327747
Sourav123 <khansourav58@gmail.com>
2022-04-05 03:22:29 +0600

    home-dome

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import useReview from '../../Hooks/useReview';
-import image from '../../404.jpg'
+import image from '../../punjabi.png'
 import Review from '../Review/Review';
 import { useNavigate } from 'react-router-dom';
+import './Home.css'
 const Home = () => {
     const [review,setReview] = useReview()
     
@@ -13,24 +14,31 @@ const navigate=useNavigate()
         <>
         <div className="flex  items-center mt-10 flex-col-reverse md:flex-row">
             <div className="px-10">
-            <h1 className="text-4xl">Your next phone</h1>
-            <h1 className="text-4xl">Your next phone</h1>
-            <p className="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, aut. Odio assumenda ipsam ratione dolor qui magnam sint amet harum velit nobis a necessitatibus, repellat asperiores quam fugit voluptate vitae deserunt. Consequatur aliquid laudantium eos deleniti reprehenderit dolor maxime amet quos fuga praesentium placeat tenetur eveniet, fugiat explicabo distinctio. Accusamus?</p>
-            <button>Live demo</button>
+            <h1 className="text-5xl text-emerald-800 "style={{fontFamily:'Times New Roman'}}>Make This Eid Charming</h1>
+            <h1 className="text-4xl text-cyan-800 mt-10" style={{fontFamily:'Times New Roman'}}>By Wearing This Punjabi</h1>
+            <p className="text-sm para-edit">Our Punjabi is made of very good quality fabric. We have our own factory. After a lot of experimentation, we have brought for you the best Punjabi of this Eid</p>
+            <button className='button-edit mt-10'>Live demo</button>
             </div>
-            <img src={image} alt=""/>
+            <img src={image} className='image-edit'alt=""/>
+            
+       
+      
         </div>
-<div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10 w-2/3 mx-auto md:w-full">
-{
+  <div className="review-1">
+  <h1 style={{marginLeft:"20px"}}>Our Top Customers Review</h1>
+      </div>
+<div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10 w-2/3 mx-auto  md:w-full">
 
+{
+    
 review.slice(0,3).map(half=><Review  
  key={half.id}
  send={half}></Review>)
 
 }
 </div>
-<button onClick={() =>navigate('/review')}>
-Show
+<button className="happy"onClick={() =>navigate('/review')}>
+Happy Clients
 </button>
         </>
 
